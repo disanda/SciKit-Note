ffe631424808b455fbb8d5d011af2fb891fd2a9a
disanda <skyanda1@qq.com>
2021-03-23 21:33:38 +0800

    change plane

--- PCA.py
+++ PCA.py
@@ -24,8 +24,8 @@ def load_data(data_folder):  #加载本地mnist数据集
 
     return (x_train, y_train), (x_test, y_test)
     
-(train_images, train_labels), (test_images, test_labels) = load_data('/Users/apple/Desktop/dataSet/Mnist_FashionMnist/MNIST/raw/')
-
+#(train_images, train_labels), (test_images, test_labels) = load_data('/Users/apple/Desktop/dataSet/Mnist_FashionMnist/MNIST/raw/')
+(train_images, train_labels), (test_images, test_labels) = load_data('/home/disanda/Desktop/dataSet/Mnist/')
 #print("original training data shape:",train_images.shape) # [60000,28,28)]
 #print("original testing data shape:",test_images.shape) # [10000,28,28]
 train_data=train_images.reshape(60000,784) #变形
@@ -33,21 +33,24 @@ test_data=test_images.reshape(10000,784)
 
 
 #------------------------降维-----------------------
-from sklearn.decomposition import PCA
-pca = PCA(n_components = 2)
-pca.fit(train_data) #fit PCA with training data instead of the whole dataset
-train_data_pca = pca.transform(train_data)
-test_data_pca = pca.transform(test_data)
-print("PCA completed with 100 components")
-print ("training data shape after PCA:",train_data_pca.shape)
-print ("testing data shape after PCA:",test_data_pca.shape)
+#from sklearn.decomposition import PCA
+#reduc = PCA(n_components = 2)
+
+
 
 from sklearn.manifold import TSNE
-tsne = TSNE(n_components=2)
+reduc = TSNE(n_components=2)
 
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+#from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 
+reduc.fit(train_data) #fit PCA with training data instead of the whole dataset
+train_data_pca = reduc.transform(train_data)
+test_data_pca = reduc.transform(test_data)
+
+print("Demention Reduction completed")
+print ("training data shape :",train_data_pca.shape)
+print ("testing data shape:",test_data_pca.shape)
 
 #------------------------分类-------------------------
 from sklearn.neighbors import KNeighborsClassifier #对降维后的mnist进行KNN分类
